67523a1636cbc7d8bfdcc8dc750c74e10af3acc5
Krishan Kumawat <krishan.kumawat@thinkvibes.com>
2023-05-26 22:12:34 +0530

    api2 commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
+"use client";
 import './globals.css'
 import { Inter } from 'next/font/google'
-// import { useState } from "react";
-
+import { useEffect } from "react";
 
 const inter = Inter({ subsets: ['latin'] })
 
